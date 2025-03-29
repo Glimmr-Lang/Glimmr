6f8bddbc51d85c79160e7438c6e426e194aadfaf
hexaredecimal <mfanakagama@gmail.com>
2025-03-29 14:07:44 +0200

    types: Added new type classes

--- src/roy/types/ObjectType.java
+++ src/roy/types/ObjectType.java
@@ -1,14 +1,44 @@
 package roy.types;
 
+import java.util.HashMap;
+import java.util.Map;
+import roy.ast.Ast;
+import roy.tokens.Token;
+
 /**
  *
  * @author hexaredecimal
  */
-public class NumberType extends Type{
+public class ObjectType extends Type {
+	public HashMap<Token, Type> fields;
+
+	public ObjectType(HashMap<Token, Type> obj) {
+		this.fields = obj;
+	}
+
 
 	@Override
 	public String toString() {
-		return "Number";
+
+		StringBuilder sb = new StringBuilder()
+			.append("{ ");
+
+		if (fields.isEmpty()) {
+			return "{}";
+		}
+
+		var entries = fields.entrySet().toArray();
+		var size = entries.length;
+		for (int i = 0; i < size; i++) {
+			var kv = (Map.Entry<Token, Ast>) entries[i];
+			sb.append(String.format("%s : %s", kv.getKey().text, kv.getValue()));
+			if (i < size - 1) {
+				sb.append(", ");
+			} 
+		}
+
+		sb.append(" }");
+		return sb.toString();
 	}
-	
+
 }
--- src/roy/types/TupleType.java
+++ src/roy/types/TupleType.java
@@ -1,14 +1,33 @@
 package roy.types;
 
+import java.util.List;
+
 /**
  *
  * @author hexaredecimal
  */
-public class NumberType extends Type{
+public class TupleType extends Type{
+	public List<Type> nodes; 
+
+	public TupleType(List<Type> nodes) {
+		this.nodes = nodes;
+	}
 
 	@Override
 	public String toString() {
-		return "Number";
+		StringBuilder sb = new StringBuilder();
+		sb.append("(");
+		for (int i = 0; i < nodes.size(); i++) {
+			var top = nodes.get(i);
+			sb.append(top);
+
+			if (i < nodes.size() - 1) {
+				sb.append(", ");
+			}
+		}
+
+		sb.append(")");
+		return sb.toString();
 	}
 	
 }
