57da196879f655a0460ebb764a4962024c5a7bf9
hexaredecimal <mfanakagama@gmail.com>
2025-04-21 03:13:48 +0200

    codegen: Fix the codegen for the situation when a function has a let as its last statement in the body

--- src/main/java/roy/codegen/jsast/JClosure.java
+++ src/main/java/roy/codegen/jsast/JClosure.java
@@ -20,20 +20,36 @@ public class JClosure implements CodegenAst {
 		StringBuilder sb = new StringBuilder();
 		sb
 			.append("function (")
-			.append(arg.toString())
+			.append(arg)
 			.append(") {")
 			.append("\n");
 		if (no_return) {
 			sb.append(body.toString().indent(4));
 		} else {
 			if (body instanceof JStringCodeEmbed) {
-				sb.append(String.format("return %s", body).toString().indent(4));
-			} else {
-				sb.append(String.format("return %s;", body).toString().indent(4));
+				sb.append(String.format("return %s", body).indent(4));
+			} else if (body instanceof JStatementList jsl) {
+				sb.append(renderStatementList(jsl));
+			}else {
+				sb.append(String.format("return %s;", body).indent(4));
 			}
 		}
 		sb.append("}");
 		return sb.toString();
 	}
 
+	private String renderStatementList(JStatementList jsl) {
+		var sb = new StringBuilder();
+		var last = jsl.statements.removeLast();
+		for (var stmt : jsl.statements) {
+			sb.append(stmt.toString().indent(4));
+		}
+
+		if (last instanceof JStatementList jsl2) {
+			sb.append("\n").append(renderStatementList(jsl2));
+		} else {
+			sb.append(String.format("return %s", last).indent(4));
+		}
+		return sb.toString();
+	}
 }
--- src/main/java/roy/codegen/jsast/JFunction.java
+++ src/main/java/roy/codegen/jsast/JFunction.java
@@ -1,10 +1,13 @@
 package roy.codegen.jsast;
 
+import java.util.List;
+
 /**
  *
  * @author hexaredecimal
  */
 public class JFunction implements CodegenAst {
+
 	public String name, arg;
 	public CodegenAst body;
 	public boolean export;
@@ -19,44 +22,61 @@ public class JFunction implements CodegenAst {
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		
+
 		if (export) {
 			sb.append("export ");
 		}
 
 		sb
-			.append("function ")
-			.append(name)
-			.append("("); 
-			
-			if (arg != null) sb.append(arg.toString());
-			
-			sb.append(") {")
-			.append("\n");
-			
-			if (body instanceof JStringCodeEmbed) {
-				sb.append(String.format(" %s", body).toString().indent(4));
-			} else  if (body instanceof JStatementList jsl) {
-				var last = jsl.statements.removeLast();
-				for (var stmt: jsl.statements) {
-					sb.append(stmt.toString().indent(4));
+						.append("function ")
+						.append(name)
+						.append("(");
+
+		if (arg != null) {
+			sb.append(arg);
+		}
+
+		sb.append(") {")
+						.append("\n");
+
+		if (body instanceof JStringCodeEmbed) {
+			sb.append(String.format(" %s", body).indent(4));
+		} else if (body instanceof JStatementList jsl) {
+			sb.append(renderStatementList(jsl));
+		} else if (body instanceof JBlock block) {
+			var last = block.statements.removeLast();
+			for (var stmt : block.statements) {
+				var s = stmt.toString();
+				sb.append(s.indent(4));
+				if (!s.endsWith(";")) {
+					sb.append(";");
 				}
-				sb.append(String.format("return %s", last).toString().indent(4));
-			} else if (body instanceof JBlock block) {
-				var last = block.statements.removeLast();
-				for (var stmt: block.statements) {
-					var s = stmt.toString();
-					sb.append(s.indent(4));
-					if (!s.endsWith(";")) sb.append(";");
-				}
-				sb.append(String.format("return %s", last).toString().indent(4));
 			}
-			else {
-				sb.append(String.format("return %s;", body).toString().indent(4));
-			}
-			
-			sb.append("}");
+			sb.append(String.format("return %s", last).indent(4));
+		} else if (body instanceof JLetExpression lt) {
+			var stmts = List.of((CodegenAst) lt);
+			var li = new JStatementList(stmts);
+			sb.append(li);
+		} else {
+			sb.append(String.format("return %s;", body).indent(4));
+		}
+
+		sb.append("}");
 		return sb.toString();
 	}
 
+	private String renderStatementList(JStatementList jsl) {
+		var sb = new StringBuilder();
+		var last = jsl.statements.removeLast();
+		for (var stmt : jsl.statements) {
+			sb.append(stmt.toString().indent(4));
+		}
+
+		if (last instanceof JStatementList jsl2) {
+			sb.append("\n").append(renderStatementList(jsl2));
+		} else {
+			sb.append(String.format("return %s", last).indent(4));
+		}
+		return sb.toString();
+	}
 }
